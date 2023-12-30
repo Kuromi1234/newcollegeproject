b3df087b397a433d5ff2f8e009ad6330c72028c0
Kuromi1234 <150152985+Kuromi1234@users.noreply.github.com>
2023-12-30 21:37:54 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2018 Brought to You By <b>Arjun</b> , <b>Manjit</b> & <b>Rabi</b></strong>
+    </div>
+</footer>
